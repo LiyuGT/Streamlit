2b82e7111f25f14088e4cd3c3d90b046810dae1f
LiyuGT <liyutenaw@gmail.com>
2024-03-14 19:12:06 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 st.write("Hello World")
 st.write("Welcome to Liyu's world")
-st.text_input("What's you name?")
-x = st.text_input("What's you name?")
+
+x = st.text_input("What's your name?")
 st.write(f"Oh, Hi {x}!")
