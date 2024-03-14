a3d6a1536b8efac4196f46d526795be187b9482a
LiyuGT <liyutenaw@gmail.com>
2024-03-14 19:14:02 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,5 +3,6 @@ st.write("Hello World")
 st.write("Welcome to Liyu's world")
 
 x = st.text_input("What's your name?")
-st.write(f"Oh, Hi {x}!")
 is_clicked = st.button("Send")
+st.write(f"Oh, Hi {x}!")
+
